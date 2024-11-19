e8febc4f27d4e3d31f8445f3d40a7b70534c1d7a
chaitali72 <chaitalismahida@gmail.com>
2024-11-18 21:22:14 -0700

    add build folder

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,13 +1,6 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
-module.exports = async function (env, argv) {
+module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-
-  // Add fallback configuration for `crypto`
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    crypto: require.resolve('crypto-browserify'),
-  };
-
   return config;
 };
